0c01a3bc50f0819bf1d780260f02299bf9a6a93e
Mikael <mikael@tuf17.mikaelhelin.com>
2024-11-11 19:42:57 +0100

    update

--- main.exs
+++ main.exs
@@ -1,37 +1,46 @@
-# ping_pong.exs
-
 defmodule PingPong do
-  # Starts the ping-pong process with an initial counter
-  def start(counter \\ 1) do
-    IO.puts("Node #{Node.self()} started with counter: #{counter}")
-    loop(counter)
+  @moduledoc """
+  A distributed Ping-Pong game where a number is passed between containers, incrementing each time.
+  """
+
+  @max_number 10
+  @delay 1000  # 1-second delay
+
+  def start(initial_number \\ 1) do
+    # Only start the game from container 1
+    if System.get_env("CONTAINER_NAME") == "elixir_container_1" do
+      send_random(initial_number)
+    else
+      listen()
+    end
   end
 
-  defp loop(counter) do
-    # Print the current state
-    IO.puts("Node #{Node.self()} has counter: #{counter}")
-
-    # Increment the counter and reset to 1 if it exceeds 11
-    next_counter = if counter >= 11, do: 1, else: counter + 1
-
-    # Get a list of all connected nodes, excluding the current node
-    nodes = Node.list() |> Enum.reject(&(&1 == Node.self()))
-
-    if nodes == [] do
-      IO.puts("No other nodes to send to. Restarting...")
-      :timer.sleep(1000)
-      loop(next_counter)
-    else
-      # Choose a random node and send the next counter
-      random_node = Enum.random(nodes)
-      IO.puts("Node #{Node.self()} sending counter #{next_counter} to #{random_node}")
-
-      # Spawn the next process on the chosen random node
-      Node.spawn(random_node, PingPong, :start, [next_counter])
-
-      # Halt this node's process as the counter is sent
-      :timer.sleep(:infinity)
+  defp listen do
+    receive do
+      {:ping, number} ->
+        IO.puts("Container #{Node.self()} received number #{number}")
+        Process.sleep(@delay)
+        send_random(increment(number))
+        listen()
     end
+  end
+
+  defp send_random(number) do
+    # Get list of all nodes, excluding itself
+    nodes = Node.list() |> Enum.reject(&(&1 == Node.self()))
+    if nodes == [] do
+      IO.puts("No other containers to send to.")
+    else
+      # Select a random node to send the message
+      target_node = Enum.random(nodes)
+      IO.puts("Container #{Node.self()} sending #{number} to #{target_node}")
+      Node.spawn(target_node, __MODULE__, :listen, [])
+      send {target_node, __MODULE__}, {:ping, number}
+    end
+  end
+
+  defp increment(number) do
+    if number >= @max_number, do: 1, else: number + 1
   end
 end
 
